3fb7a43b02a77675f886fdfe79fe39ddb5572a5d
kwyoohae <gocksdl7979@gmail.com>
2023-07-03 17:21:23 +0900

    style: Assertions static import 추가

--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -4,12 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PawnTest {
 
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
         Pawn pawn = new Pawn("white");
-        Assertions.assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
     }
 }
