511ce4604250629c86c4395530cd171c498696a4
kwyoohae <gocksdl7979@gmail.com>
2023-07-04 10:22:17 +0900

    refactor: 상수로 값 하드코딩 변경 및 생성자 순서 변경

--- src/main/java/softeer2nd/Pawn.java
+++ src/main/java/softeer2nd/Pawn.java
@@ -2,15 +2,17 @@ package softeer2nd;
 
 public class Pawn {
 
+    private final String WHITE_COLOR = "white";
+
     private final String color;
 
+    public Pawn() {
+        this.color = WHITE_COLOR;
+    }
     public Pawn(final String color) {
         this.color = color;
     }
 
-    public Pawn() {
-        this.color = "white";
-    }
 
     public String getColor() {
         return color;
