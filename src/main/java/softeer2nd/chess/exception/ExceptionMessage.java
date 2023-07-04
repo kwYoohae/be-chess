046d4ec77cf210080f1215cfb4ad836e97638e86
kwyoohae <gocksdl7979@gmail.com>
2023-07-04 11:21:33 +0900

    refactor: ExceptionMessage를 인스턴스화 못하도록 막기

--- src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
+++ src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
@@ -2,4 +2,9 @@ package softeer2nd.chess.exception;
 
 public final class ExceptionMessage {
 	public static final String BOARD_HAS_NOT_OVER_THE_SAVE_PAWN = "저장된 갯수보다 많은 Pawn은 불러올 수 없습니다";
+	public static final String EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE = "ExceptionMessage는 인스턴스화 할 수 없는 유틸 클래스입니다";
+
+	private ExceptionMessage() {
+		throw new IllegalStateException(EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE);
+	}
 }
