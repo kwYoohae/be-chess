980879be610c5727881c44ec187fa914f0cb246f
kwyoohae <gocksdl7979@gmail.com>
2023-07-04 11:38:38 +0900

    feat: Pawn에 representaion 생성

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -4,18 +4,26 @@ public class Pawn {
 
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
 
     private final String color;
+    private final char representation;
 
     public Pawn() {
-        this(WHITE_COLOR);
-    }
-    public Pawn(final String color) {
-        this.color = color;
+        this(WHITE_COLOR, WHITE_REPRESENTATION);
     }
 
+    public Pawn(final String color, final char representation) {
+        this.color = color;
+        this.representation = representation;
+    }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 }
