d812ab66b5d5601dae89929c905b516db0629c83
kwyoohae <gocksdl7979@gmail.com>
2023-07-04 17:44:24 +0900

    fix: 오탈자 제거

--- src/test/java/softeer2nd/HelloTest.java
+++ src/test/java/softeer2nd/HelloTest.java
@@ -2,9 +2,6 @@ package softeer2nd;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +12,5 @@ class HelloTest {
     void hello() {
         int a = 777;
         assertThat(a).isEqualTo(777);
-
-        Map<String, String> map = new HashMap<>();
-        assertThat(map.get("a")).isEqualTo("a");
     }
 }
