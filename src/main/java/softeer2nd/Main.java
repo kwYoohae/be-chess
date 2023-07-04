37ef92095a6a4c265e30744824a475a8487a899c
kwyoohae <gocksdl7979@gmail.com>
2023-07-04 18:26:11 +0900

    feat: 시작 및 종료 로직 작성

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -1,7 +1,28 @@
 package softeer2nd;
 
+import static softeer2nd.chess.exception.ExceptionMessage.*;
+
+import java.util.Scanner;
+
+import softeer2nd.chess.Board;
+
 public class Main {
+
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.next();성
+            if (command.equals(START_COMMAND)) {
+                final Board board = new Board();
+                board.print();
+            } else if (command.equals(END_COMMAND)) {
+                break;
+            } else {
+                throw new IllegalArgumentException(NOT_EXECUTE_COMMAND);
+            }
+        }
     }
 }
--- src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
+++ src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
@@ -4,6 +4,7 @@ public final class ExceptionMessage {
 	public static final String EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE = "ExceptionMessage는 인스턴스화 할 수 없는 유틸 클래스입니다";
 	public static final String EXCEPTION_MESSAGE_CHESS_COLOR_MUST_BLACK_OR_WHITE = "체스 기물의 색은 BLACK 또는 WHITE만 존재합니다";
 	public static final String DO_NOT_FIND_PAWN_IN_BOARD = "보드에서 Pawn을 찾을 수 없습니다";
+	public static final String NOT_EXECUTE_COMMAND = "실행 할 수 없는 명령어 입니다";
 
 	private ExceptionMessage() {
 		throw new IllegalStateException(EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE);
