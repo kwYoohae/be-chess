6ae620343bad0624e3a7a3f111d76e4a1ccec11a
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 10:30:53 +0900

    refactor: VO로 Piece 클래스 변경

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -5,40 +5,16 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import softeer2nd.chess.exception.ExceptionMessage;
-
 class PieceTest {
 
     @Test
     @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
-    void create() {
-        verifyPawn(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+    void create_piece() {
+        verifyPawn(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPawn(Piece.createBlackPawn() ,Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
     }
 
-    @Test
-    void create_기본생성자() {
-        // given
-        Piece piece = new Piece();
-
-        // when, then
-        assertThat(piece.getColor()).isEqualTo(Piece.WHITE_COLOR);
-        assertThat(piece.getRepresentation()).isEqualTo(Piece.WHITE_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("폰은 검정색과 흰색으로만 이루어져야한다")
-    void pawnIsOnlyBlackAndWhite() {
-        assertThatThrownBy(() -> new Piece("yellow"))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage(ExceptionMessage.EXCEPTION_MESSAGE_CHESS_COLOR_MUST_BLACK_OR_WHITE);
-    }
-
-    void verifyPawn(final String color, final char representation) {
-        //given
-        final Piece piece = new Piece(color);
-
-        //when, then
+    void verifyPawn(final Piece piece, final String color, final char representation) {
         assertThat(piece.getColor()).isEqualTo(color);
         assertThat(piece.getRepresentation()).isEqualTo(representation);
     }
