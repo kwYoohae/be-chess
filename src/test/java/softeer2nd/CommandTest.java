13c19588072a0fd34baff2b67363ffaad538d1f6
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 09:24:32 +0900

    test: 정하지 않은 명령어를 입력할시 에러 발생하는 로직 작성

---
+++ src/test/java/softeer2nd/CommandTest.java
@@ -0,0 +1,30 @@
+package softeer2nd;
+
+import static softeer2nd.chess.exception.ExceptionMessage.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CommandTest {
+
+	@Test
+	@DisplayName("만약, 정해져있지 않은 명령어를 입력하면 오류가 발생한다")
+	void checkNotDeclareCommand() {
+		// given
+		String input = "not a command\n";
+		final InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		Command command = new Command(new Scanner(in));
+
+		// when, then
+		Assertions.assertThatThrownBy(command::getUserInput)
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage(NOT_EXECUTE_COMMAND);
+
+	}
+}
