6eb4000dd4517c47bc66ad7e2b8adeec09301b0d
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 09:12:53 +0900

    refactor: Command class로 input에 대한 검증 및 상수 로직 이전

---
+++ src/main/java/softeer2nd/Command.java
@@ -0,0 +1,31 @@
+package softeer2nd;
+
+import static softeer2nd.chess.exception.ExceptionMessage.*;
+
+import java.util.Scanner;
+
+public class Command {
+
+	private static final String ASK_COMMAND_MESSAGE = "명령어를 입력해주세요 : ";
+	public static final String START_COMMAND = "start";
+	public static final String END_COMMAND = "end";
+
+	private final Scanner scanner;
+
+	public Command(final Scanner scanner) {
+		this.scanner = scanner;
+	}
+
+	public String getUserInput() {
+		System.out.print(ASK_COMMAND_MESSAGE);
+		String input = scanner.nextLine();
+		validateUserInput(input);
+		return input;
+	}
+
+	private void validateUserInput(String input) {
+		if (!input.equals(START_COMMAND) && !input.equals(END_COMMAND)) {
+			throw new IllegalArgumentException(NOT_EXECUTE_COMMAND);
+		}
+	}
+}
--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -1,6 +1,6 @@
 package softeer2nd;
 
-import static softeer2nd.chess.exception.ExceptionMessage.*;
+import static softeer2nd.Command.*;
 
 import java.util.Scanner;
 
@@ -8,27 +8,22 @@ import softeer2nd.chess.Board;
 
 public class Main {
 
-	private static final String ASK_COMMAND_MESSAGE = "명령어를 입력해주세요 : ";
 	private static final String START_GAME_MESSAGE = "게임을 시작합니다";
 	private static final String END_GAME_MESSAGE = "게임을 종료합니다";
-	private static final String START_COMMAND = "start";
-	private static final String END_COMMAND = "end";
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Command command = new Command(new Scanner(System.in));
+
 		while (true) {
-			System.out.print(ASK_COMMAND_MESSAGE);
-			String command = sc.next();
-			if (command.equals(START_COMMAND)) {
+			String input = command.getUserInput();
+			if (input.equals(START_COMMAND)) {
 				System.out.println(START_GAME_MESSAGE);
 				final Board board = new Board();
 				board.initialize();
 				System.out.println(board.print());
-			} else if (command.equals(END_COMMAND)) {
+			} else if (input.equals(END_COMMAND)) {
 				System.out.println(END_GAME_MESSAGE);
 				break;
-			} else {
-				throw new IllegalArgumentException(NOT_EXECUTE_COMMAND);
 			}
 		}
 	}
