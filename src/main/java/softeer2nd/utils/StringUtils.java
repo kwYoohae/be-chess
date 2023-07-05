79a2dbb47b593867adb9afb631b134102bcb5b95
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 09:47:03 +0900

    feat: StringUtils의 appendLine을 검증하는 테스트 코드 작성

---
+++ src/main/java/softeer2nd/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package softeer2nd.utils;
+
+public class StringUtils {
+
+	private static final String NEWLINE = System.getProperty("line.separator");
+
+	private StringUtils() {}
+
+	public static String appendLine(final String input) {
+		return null;
+	}
+}
