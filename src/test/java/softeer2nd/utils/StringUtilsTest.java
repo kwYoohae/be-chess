0fa159a26140c943b170f8e7320f19e55c9321ce
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 09:54:27 +0900

    feat: StringUtils 검증로직 작성

--- src/test/java/softeer2nd/utils/StringUtilsTest.java
+++ src/test/java/softeer2nd/utils/StringUtilsTest.java
@@ -4,17 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import softeer2nd.utils.StringUtils;
-
-public class StringUtilsTest {
+class StringUtilsTest {
 
 	@Test
 	@DisplayName("appendLine은 제대로 문자끝에 개행을 추가 할 수 있어야한다")
 	void appendLine() {
 		// given
 		String input = "........";
-		String answer = "........\n";
+		String answer = "........" + StringUtils.NEWLINE;
 		// when, then
-		Assertions.assertThat(StringUtils.appendLine(input)).isEqualTo(input + System.getProperty(answer));
+		Assertions.assertThat(StringUtils.appendLine(input)).isEqualTo(answer);
 	}
 }
