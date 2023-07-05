1657b219c1c571392f5c2c152fa7d76c3a5f3e48
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 23:20:12 +0900

    refactor: Board를 저장하는 저장소를 ArrayList로 변경

--- src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
+++ src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
@@ -3,6 +3,7 @@ package softeer2nd.chess.exception;
 public final class ExceptionMessage {
 	public static final String EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE = "ExceptionMessage는 인스턴스화 할 수 없는 유틸 클래스입니다";
 	public static final String NOT_EXECUTE_COMMAND = "실행 할 수 없는 명령어 입니다";
+	public static final String NOT_EXIST_PIECE = "존재하지 않는 기물입니다";
 
 	private ExceptionMessage() {
 		throw new IllegalStateException(EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE);
