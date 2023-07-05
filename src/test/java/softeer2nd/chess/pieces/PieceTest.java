9c1491c2a251de33a0697aa5b8eb2feda359b44a
kwyoohae <gocksdl7979@gmail.com>
2023-07-05 10:42:00 +0900

    test: 기물 추가로 인한 테스트 항목 추가

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -8,10 +8,25 @@ import org.junit.jupiter.api.Test;
 class PieceTest {
 
     @Test
-    @DisplayName("흰색과 검은색 폰이 생성되어야 한다")
+    @DisplayName("흰색과 검은색 기물들이 모두 생성 되어야 한다")
     void create_piece() {
-        verifyPawn(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPawn(Piece.createBlackPawn() ,Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+        verifyPawn(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPawn(Piece.createBlackPawn() ,Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+
+        verifyPawn(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPawn(Piece.createBlackKnight() ,Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+
+        verifyPawn(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPawn(Piece.createBlackRook() ,Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPawn(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPawn(Piece.createBlackBishop() ,Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPawn(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPawn(Piece.createBlackQueen() ,Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+
+        verifyPawn(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPawn(Piece.createBlackKing() ,Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
     void verifyPawn(final Piece piece, final String color, final char representation) {
