72c5710cafa6d9ecdab4d947ff76f263890ce309
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 10:35:53 +0900

    refactor: Command관련 기능을 Enum으로 분리

--- src/main/java/softeer2nd/chess/domain/game/Game.java
+++ src/main/java/softeer2nd/chess/domain/game/Game.java
@@ -1,8 +1,7 @@
 package softeer2nd.chess.domain.game;
 
-import static softeer2nd.chess.view.InputView.*;
-
 import softeer2nd.chess.domain.board.Board;
+import softeer2nd.chess.view.Command;
 import softeer2nd.chess.view.InputView;
 
 public class Game {
@@ -10,21 +9,21 @@ public class Game {
 	private static final String START_GAME_MESSAGE = "게임을 시작합니다";
 	private static final String END_GAME_MESSAGE = "게임을 종료합니다";
 
-	private final InputView command;
+	private final InputView inputView;
 
-	public Game(final InputView command) {
-		this.command = command;
+	public Game(final InputView inputView) {
+		this.inputView = inputView;
 	}
 
 	public void startGame() {
 		while (true) {
-			String input = command.getUserInput();
-			if (input.equals(START_COMMAND)) {
+			Command command = inputView.getUserInput();
+			if (command == Command.START) {
 				System.out.println(START_GAME_MESSAGE);
 				final Board board = new Board();
 				board.initialize();
 				System.out.println(board.showBoard());
-			} else if (input.equals(END_COMMAND)) {
+			} else if (command == Command.END) {
 				System.out.println(END_GAME_MESSAGE);
 				break;
 			}
--- src/main/java/softeer2nd/chess/view/InputView.java
+++ src/main/java/softeer2nd/chess/view/InputView.java
@@ -1,14 +1,10 @@
 package softeer2nd.chess.view;
 
-import static softeer2nd.chess.exception.ExceptionMessage.*;
-
 import java.util.Scanner;
 
 public class InputView {
 
 	private static final String ASK_COMMAND_MESSAGE = "명령어를 입력해주세요 : ";
-	public static final String START_COMMAND = "start";
-	public static final String END_COMMAND = "end";
 
 	private final Scanner scanner;
 
@@ -16,16 +12,9 @@ public class InputView {
 		this.scanner = scanner;
 	}
 
-	public String getUserInput() {
+	public Command getUserInput() {
 		System.out.print(ASK_COMMAND_MESSAGE);
 		String input = scanner.nextLine();
-		validateUserInput(input);
-		return input;
-	}
-
-	private void validateUserInput(String input) {
-		if (!input.equals(START_COMMAND) && !input.equals(END_COMMAND)) {
-			throw new IllegalArgumentException(NOT_EXECUTE_COMMAND);
-		}
+		return Command.valueOfInput(input);
 	}
 }
