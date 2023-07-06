cf5df51d8ddffb1f4ee5dc9f3d16c563f16d785b
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 11:41:23 +0900

    feat: position에 대한 input이 범위를 넘어가는지 확인하는 로직 작성

--- src/main/java/softeer2nd/chess/domain/board/Position.java
+++ src/main/java/softeer2nd/chess/domain/board/Position.java
@@ -2,9 +2,13 @@ package softeer2nd.chess.domain.board;
 
 import static softeer2nd.chess.exception.ExceptionMessage.*;
 
+import java.util.Arrays;
+
 public class Position {
 
 	private static final int POSITION_COUNT = 2;
+	private static final String[] ROW = {"a", "b", "c", "d", "e", "f", "g", "h"};
+	private static final String[] COLUMN = {"1", "2", "3", "4", "5", "6", "7", "8"};
 
 	private final String x;
 	private final String y;
@@ -23,6 +27,12 @@ public class Position {
 	private static void validatePosition(final String[] inputs) {
 		if (inputs.length != POSITION_COUNT) {
 			throw new IllegalArgumentException(POSITION_INPUT_IS_TO_MANY_OR_LESS_LENGTH);
+		}
+
+		final boolean isFirstPositionValid = Arrays.stream(ROW).anyMatch(row -> row.equals(inputs[0]));
+		final boolean isSecondPositionValid = Arrays.stream(COLUMN).anyMatch(column -> column.equals(inputs[0]));
+		if (!isFirstPositionValid || !isSecondPositionValid) {
+			throw new IllegalArgumentException(POSITION_INPUT_IS_WRONG);
 		}
 	}
 
