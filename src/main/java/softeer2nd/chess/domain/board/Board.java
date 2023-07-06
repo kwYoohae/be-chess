446d0b2b808560661dc29013c83fd23edb656504
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 10:18:01 +0900

    refactor: 패키지 구조 변경

--- src/main/java/softeer2nd/chess/domain/board/Board.java
+++ src/main/java/softeer2nd/chess/domain/board/Board.java
@@ -1,9 +1,9 @@
-package softeer2nd.chess;
-
-import static softeer2nd.chess.pieces.Piece.*;
+package softeer2nd.chess.domain.board;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import softeer2nd.chess.domain.pieces.Piece;
 
 public class Board {
 
@@ -13,21 +13,21 @@ public class Board {
 
 	public void initialize() {
 		boards.clear();
-		boards.add(Rank.initializeOtherPieces(Color.WHITE));
-		boards.add(Rank.initializePawn(Color.WHITE));
+		boards.add(Rank.initializeOtherPieces(Piece.Color.WHITE));
+		boards.add(Rank.initializePawn(Piece.Color.WHITE));
 		boards.add(Rank.initializeBlank());
 		boards.add(Rank.initializeBlank());
 		boards.add(Rank.initializeBlank());
 		boards.add(Rank.initializeBlank());
-		boards.add(Rank.initializePawn(Color.BLACK));
-		boards.add(Rank.initializeOtherPieces(Color.BLACK));
+		boards.add(Rank.initializePawn(Piece.Color.BLACK));
+		boards.add(Rank.initializeOtherPieces(Piece.Color.BLACK));
 	}
 
-	public String getPawnsResult(Color color) {
+	public String getPawnsResult(Piece.Color color) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = BOARD_MAX_INDEX - 1; i >= 0; i--) {
 			final Rank row = boards.get(i);
-			sb.append(row.showLineWantPiece(Type.PAWN, color));
+			sb.append(row.showLineWantPiece(Piece.Type.PAWN, color));
 		}
 		sb.deleteCharAt(sb.length() - 1);
 		return sb.toString();
