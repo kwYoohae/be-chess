304ce7a353b2f9332195f7096485c1cfbf551cf8
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 11:41:23 +0900

    feat: position에 대한 input이 범위를 넘어가는지 확인하는 로직 작성

---
+++ src/test/java/softeer2nd/chess/domain/board/PositionTest.java
@@ -0,0 +1,18 @@
+package softeer2nd.chess.domain.board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+	@Test
+	@DisplayName("만약, position의 범위를 벗어난 위치의 기물을 찾을때 오류가 발생한다")
+	void wrongPosition() {
+		// given
+		String input "z43";
+
+	}
+
+}
