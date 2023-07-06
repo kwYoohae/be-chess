3328d3637b230998242f028c345595e45df2dae7
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 10:56:43 +0900

    test: 원하는 기물의 갯수를 반환하는 테스트 작성

--- src/test/java/softeer2nd/chess/domain/board/BoardTest.java
+++ src/test/java/softeer2nd/chess/domain/board/BoardTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import softeer2nd.chess.domain.board.Board;
 import softeer2nd.chess.domain.pieces.Piece;
 
 class BoardTest {
@@ -40,5 +39,42 @@ class BoardTest {
 
 		assertThat(board.getPawnsResult(Piece.Color.WHITE)).isEqualTo("pppppppp");
 		assertThat(board.getPawnsResult(Piece.Color.BLACK)).isEqualTo("PPPPPPPP");
+	}
+
+	@Test
+	@DisplayName("원하는 기물의 갯수를 반환해야 한다")
+	void findWantPieceCount() {
+		// given
+		board.initialize();
+
+		// when
+		final int whiteQueen = board.pieceCount(Piece.Type.QUEEN, Piece.Color.WHITE);
+		final int blackQueen = board.pieceCount(Piece.Type.QUEEN, Piece.Color.BLACK);
+		final int whiteRook = board.pieceCount(Piece.Type.ROOK, Piece.Color.WHITE);
+		final int blackRook = board.pieceCount(Piece.Type.ROOK, Piece.Color.BLACK);
+		final int whiteKing = board.pieceCount(Piece.Type.KING, Piece.Color.WHITE);
+		final int blackKing = board.pieceCount(Piece.Type.KING, Piece.Color.BLACK);
+		final int whiteKnight = board.pieceCount(Piece.Type.KNIGHT, Piece.Color.WHITE);
+		final int blackKnight = board.pieceCount(Piece.Type.KNIGHT, Piece.Color.BLACK);
+		final int whiteBishop = board.pieceCount(Piece.Type.BISHOP, Piece.Color.WHITE);
+		final int blackBishop = board.pieceCount(Piece.Type.BISHOP, Piece.Color.BLACK);
+		final int whitePawn = board.pieceCount(Piece.Type.PAWN, Piece.Color.WHITE);
+		final int blackPawn = board.pieceCount(Piece.Type.PAWN, Piece.Color.BLACK);
+		final int blankPiece = board.pieceCount(Piece.Type.NO_PIECE, Piece.Color.NOCOLOR);
+
+		// then
+		assertThat(whiteQueen).isEqualTo(1);
+		assertThat(blackQueen).isEqualTo(1);
+		assertThat(whiteRook).isEqualTo(2);
+		assertThat(blackRook).isEqualTo(2);
+		assertThat(whiteKing).isEqualTo(1);
+		assertThat(blackKing).isEqualTo(1);
+		assertThat(whiteKnight).isEqualTo(2);
+		assertThat(blackKnight).isEqualTo(2);
+		assertThat(whiteBishop).isEqualTo(2);
+		assertThat(blackBishop).isEqualTo(2);
+		assertThat(whitePawn).isEqualTo(8);
+		assertThat(blackPawn).isEqualTo(8);
+		assertThat(blankPiece).isEqualTo(32);
 	}
 }
