946c911ec8cf9b7b814eb6ee42d278dcfadde2b1
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 10:55:52 +0900

    fix: 생성자 추가하지 않은 오류 변경

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -2,6 +2,7 @@ package softeer2nd;
 
 import java.util.Scanner;
 
+import softeer2nd.chess.domain.board.Board;
 import softeer2nd.chess.domain.game.Game;
 import softeer2nd.chess.view.InputView;
 import softeer2nd.chess.view.OutputView;
@@ -9,9 +10,10 @@ import softeer2nd.chess.view.OutputView;
 public class Main {
 
 	public static void main(String[] args) {
-		InputView inputView = new InputView(new Scanner(System.in));
-		OutputView outputView = new OutputView();
-		Game game = new Game(inputView, outputView);
+		final InputView inputView = new InputView(new Scanner(System.in));
+		final OutputView outputView = new OutputView();
+		final Board board = new Board();
+		Game game = new Game(inputView, outputView, board);
 
 		game.startGame();
 	}
