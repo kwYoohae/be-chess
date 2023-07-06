3295eba98da9761971d67423870524bccd3563b5
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 11:44:34 +0900

    test: position의 범위를 벗어난 위치의 기물을 찾을 경우는 오류가 발생합니다

--- src/test/java/softeer2nd/chess/domain/board/PositionTest.java
+++ src/test/java/softeer2nd/chess/domain/board/PositionTest.java
@@ -1,7 +1,8 @@
 package softeer2nd.chess.domain.board;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static softeer2nd.chess.exception.ExceptionMessage.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +12,12 @@ class PositionTest {
 	@DisplayName("만약, position의 범위를 벗어난 위치의 기물을 찾을때 오류가 발생한다")
 	void wrongPosition() {
 		// given
-		String input "z43";
+		String input = "z4";
 
+		// when, then
+		Assertions.assertThatThrownBy(() -> Position.from(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage(POSITION_INPUT_IS_WRONG);
 	}
 
 }
