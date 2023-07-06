f86a877f912557d3ebbae3b2dcf444f3e4e59002
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 09:07:48 +0900

    feat: row를 담당하는 Rank 클래스 생성

---
+++ src/main/java/softeer2nd/chess/Rank.java
@@ -0,0 +1,13 @@
+package softeer2nd.chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import softeer2nd.chess.pieces.Piece;
+
+public class Rank {
+
+	private final List<Piece> row = new ArrayList<>();
+
+
+}
