ebc092cab39b745ccdebead1addb35fd66eaff93
kwyoohae <gocksdl7979@gmail.com>
2023-07-06 09:53:03 +0900

    refactor: Rank로 기존코드 분리

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -1,130 +1,52 @@
 package softeer2nd.chess;
 
 import static softeer2nd.chess.pieces.Piece.*;
-import static softeer2nd.utils.StringUtils.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import softeer2nd.chess.pieces.Piece;
-
 public class Board {
 
-	private static final char EMPTY_POSITION_REPRESENTATION = '.';
 	public static final int BOARD_MAX_INDEX = 8;
 
-	private final List<List<Piece>> boards = new ArrayList<>();
+	private final List<Rank> boards = new ArrayList<>();
 
 	public void initialize() {
 		boards.clear();
-		boards.add(initializeOtherPieces(Color.WHITE));
-		boards.add(initializePawn(Color.WHITE));
-		boards.add(initializeBlank());
-		boards.add(initializeBlank());
-		boards.add(initializeBlank());
-		boards.add(initializeBlank());
-		boards.add(initializePawn(Color.BLACK));
-		boards.add(initializeOtherPieces(Color.BLACK));
-	}
-
-	public List<Piece> initializePawn(Color color) {
-		List<Piece> pawns = new ArrayList<>();
-		for (int i = 0; i < BOARD_MAX_INDEX; i++) {
-			if (color == Color.BLACK) {
-				pawns.add(Piece.createBlackPawn());
-			} else if (color == Color.WHITE) {
-				pawns.add(Piece.createWhitePawn());
-			}
-		}
-		return pawns;
-	}
-
-	public List<Piece> initializeBlank() {
-		List<Piece> blanks = new ArrayList<>();
-		for (int i = 0; i < BOARD_MAX_INDEX; i++) {
-			blanks.add(Piece.createBlank());
-		}
-
-		return blanks;
-	}
-
-	private List<Piece> initializeOtherPieces(Color color) {
-		List<Piece> pieces = new ArrayList<>();
-		pieces.add(Piece.createPiece(color, Type.ROOK));
-		pieces.add(Piece.createPiece(color, Type.KNIGHT));
-		pieces.add(Piece.createPiece(color, Type.BISHOP));
-		pieces.add(Piece.createPiece(color, Type.QUEEN));
-		pieces.add(Piece.createPiece(color, Type.KING));
-		pieces.add(Piece.createPiece(color, Type.BISHOP));
-		pieces.add(Piece.createPiece(color, Type.KNIGHT));
-		pieces.add(Piece.createPiece(color, Type.ROOK));
-		return pieces;
+		boards.add(Rank.initializeOtherPieces(Color.WHITE));
+		boards.add(Rank.initializePawn(Color.WHITE));
+		boards.add(Rank.initializeBlank());
+		boards.add(Rank.initializeBlank());
+		boards.add(Rank.initializeBlank());
+		boards.add(Rank.initializeBlank());
+		boards.add(Rank.initializePawn(Color.BLACK));
+		boards.add(Rank.initializeOtherPieces(Color.BLACK));
 	}
 
 	public String getPawnsResult(Color color) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = BOARD_MAX_INDEX - 1; i >= 0; i--) {
-			final String line = getPawnsLineSameColor(color, i);
-			if (line.contains(String.valueOf(Type.PAWN.getWhiteRepresentation())) || line.contains(
-				String.valueOf(Type.PAWN.getBlackRepresentation()))) {
-				sb.append(appendNewLine(line));
-			}
+			final Rank row = boards.get(i);
+			sb.append(row.showLineWantPiece(Type.PAWN, color));
 		}
 		sb.deleteCharAt(sb.length() - 1);
 		return sb.toString();
 	}
 
-	private String getPawnsLineSameColor(final Color color, final int rowIndex) {
-		StringBuilder line = new StringBuilder();
-		final List<Piece> rowPieces = boards.get(rowIndex);
-		for (int j = 0; j < BOARD_MAX_INDEX; j++) {
-			line.append(getPawnRepresentation(color, rowPieces.get(j)));
-		}
-		return line.toString();
-	}
-
-	private char getPawnRepresentation(final Color color, final Piece piece) {
-		if (piece != null && color == piece.getColor()) {
-			return piece.getRepresentation();
-		}
-		return EMPTY_POSITION_REPRESENTATION;
-	}
-
-	private char getPawnRepresentation(final Piece piece) {
-		if (piece != null) {
-			return piece.getRepresentation();
-		}
-		return EMPTY_POSITION_REPRESENTATION;
-	}
-
 	public String showBoard() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = BOARD_MAX_INDEX - 1; i >= 0; i--) {
-			sb.append(appendNewLine(getPawnLine(i)));
+			final Rank row = boards.get(i);
+			sb.append(row.showPieceLine());
 		}
 		return sb.toString();
 	}
 
-	private String getPawnLine(final int rowIndex) {
-		StringBuilder line = new StringBuilder();
-		final List<Piece> rowPieces = boards.get(rowIndex);
-		for (int i = 0; i < BOARD_MAX_INDEX; i++) {
-			line.append(getPawnRepresentation(rowPieces.get(i)));
-		}
-		return line.toString();
-	}
-
 	public int pieceCount() {
 		int sum = 0;
-		for (List<Piece> row : boards) {
-			sum += piecesRowCount(row);
+		for (Rank row : boards) {
+			sum += row.pieceCount();
 		}
 		return sum;
-	}
-
-	private long piecesRowCount(List<Piece> pieces) {
-		return pieces.stream().
-			filter(piece -> piece.getType() != Type.NO_PIECE)
-			.count();
 	}
 }
--- src/main/java/softeer2nd/chess/Rank.java
+++ src/main/java/softeer2nd/chess/Rank.java
@@ -1,5 +1,8 @@
 package softeer2nd.chess;
 
+import static softeer2nd.chess.pieces.Piece.*;
+import static softeer2nd.utils.StringUtils.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +10,83 @@ import softeer2nd.chess.pieces.Piece;
 
 public class Rank {
 
-	private final List<Piece> row = new ArrayList<>();
+	private static final int MAX_ROW = 8;
+	private static final String BLACK_LINE = "........";
+	private static final String EMPTY_STRING = "";
+	private final List<Piece> row;
 
+	private Rank(List<Piece> row) {
+		this.row = row;
+	}
 
+	public static Rank initializePawn(Color color) {
+		List<Piece> pawns = new ArrayList<>();
+		for (int i = 0; i < MAX_ROW; i++) {
+			if (color == Color.BLACK) {
+				pawns.add(createBlackPawn());
+			} else if (color == Color.WHITE) {
+				pawns.add(createWhitePawn());
+			}
+		}
+
+		return new Rank(pawns);
+	}
+
+	public static Rank initializeBlank() {
+		List<Piece> blanks = new ArrayList<>();
+		for (int i = 0; i < MAX_ROW; i++) {
+			blanks.add(createBlank());
+		}
+
+		return new Rank(blanks);
+	}
+
+	public static Rank initializeOtherPieces(Color color) {
+		List<Piece> pieces = new ArrayList<>();
+		pieces.add(createPiece(color, Type.ROOK));
+		pieces.add(createPiece(color, Type.KNIGHT));
+		pieces.add(createPiece(color, Type.BISHOP));
+		pieces.add(createPiece(color, Type.QUEEN));
+		pieces.add(createPiece(color, Type.KING));
+		pieces.add(createPiece(color, Type.BISHOP));
+		pieces.add(createPiece(color, Type.KNIGHT));
+		pieces.add(createPiece(color, Type.ROOK));
+
+		return new Rank(pieces);
+	}
+
+	public long pieceCount() {
+		return row.stream()
+			.filter(piece -> piece.getType() != Type.NO_PIECE)
+			.count();
+	}
+
+	public String showPieceLine() {
+		StringBuilder line = new StringBuilder();
+		for (Piece piece : row) {
+			line.append(piece.getRepresentation());
+		}
+		return appendNewLine(line.toString());
+	}
+
+	public String showLineWantPiece(Type type, Color color) {
+		final String line = getLineSamePiece(type, color);
+		if (line.equals(BLACK_LINE)) {
+			return EMPTY_STRING;
+		}
+		return appendNewLine(line);
+	}
+
+	private String getLineSamePiece(Type type, Color color) {
+		StringBuilder line = new StringBuilder();
+		for (int i = 0; i < MAX_ROW; i++) {
+			final Piece piece = row.get(i);
+			if (piece.getColor() == color && piece.getType() == type) {
+				line.append(piece.getRepresentation());
+				continue;
+			}
+			line.append(Type.NO_PIECE.getBlackRepresentation());
+		}
+		return line.toString();
+	}
 }
