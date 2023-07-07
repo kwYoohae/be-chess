4cb45b135c5534e2b8bab1726d1cace305b901f1
kwyoohae <gocksdl7979@gmail.com>
2023-07-07 11:26:20 +0900

    test: 존재하지 않은 position의 값을 반환하면 오류가 발생하는 테스트 작성

---
+++ src/test/java/softeer2nd/chess/domain/board/position/RowTest.java
@@ -0,0 +1,21 @@
+package softeer2nd.chess.domain.board.position;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RowTest {
+
+	@Test
+	@DisplayName("존재하지 않은 position의 값을 반환할때 오류가 발생한다")
+	void valueOfPosition() {
+		// given
+		char targetPosition = 'z';
+
+		// when, then
+		assertThrows(IllegalArgumentException.class,() -> {
+			Row.valueOfPosition(targetPosition);
+		});
+	}
+}
