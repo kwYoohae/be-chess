ffacb59d79826b0ddbe7e9a2c3bcc190ad264e1d
kwyoohae <gocksdl7979@gmail.com>
2023-07-07 11:53:50 +0900

    feat: 기물을 명령어로 움직일 수 있게 하는 로직 구현

--- src/main/java/softeer2nd/chess/view/Command.java
+++ src/main/java/softeer2nd/chess/view/Command.java
@@ -5,7 +5,7 @@ import static softeer2nd.chess.exception.ExceptionMessage.*;
 import java.util.Arrays;
 
 public enum Command {
-	START("start"), END("end");
+	START("start"), END("end"), MOVE("move");
 
 	private final String command;
 
@@ -18,9 +18,33 @@ public enum Command {
 	}
 
 	public static Command valueOfInput(String input) {
+		final String[] inputs = input.split(" ");
 		return Arrays.stream(values())
-			.filter(value -> value.command.equals(input))
+			.filter(value -> value.command.equals(inputs[0]))
 			.findAny()
 			.orElseThrow(() -> new IllegalArgumentException(NOT_EXECUTE_COMMAND));
+	}
+
+	public static void validationCommand(String input) {
+		final String[] inputs = input.split(" ");
+
+		validateCommandLength(inputs);
+		validateCommand(inputs);
+	}
+
+	private static void validateCommandLength(String[] inputs) {
+		if (inputs.length != 1 && inputs.length != 3) {
+			throw new IllegalArgumentException(NOT_EXECUTE_COMMAND);
+		}
+	}
+
+	private static void validateCommand(String[] inputs) {
+		if (inputs.length == 1 && (inputs[0].equals(START.command) || inputs[0].equals(END.command))) {
+			return;
+		}
+		if (inputs.length == 3 && inputs[0].equals(MOVE.command))
+			return;
+
+		throw new IllegalArgumentException(NOT_EXECUTE_COMMAND);
 	}
 }
