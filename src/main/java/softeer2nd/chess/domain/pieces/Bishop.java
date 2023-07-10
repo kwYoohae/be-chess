9262b1e319fb78d5373dc75da08ef1f9fc7384c6
kwyoohae <gocksdl7979@gmail.com>
2023-07-10 13:09:52 +0900

    feat: Bishop이 갈 수 있는 곳을 확인하는 로직작성

--- src/main/java/softeer2nd/chess/domain/pieces/Bishop.java
+++ src/main/java/softeer2nd/chess/domain/pieces/Bishop.java
@@ -1,16 +1,42 @@
 package softeer2nd.chess.domain.pieces;
 
 import softeer2nd.chess.domain.board.position.Position;
+import softeer2nd.chess.exception.ExceptionMessage;
 
 public class Bishop extends Piece{
 	protected Bishop(Color color, Type type, Position position) {
 		this.color = color;
 		this.type = type;
 		this.position = position;
+		this.directions = Direction.diagonalDirection();
 	}
 
 	@Override
 	public void checkPieceCanGo(final Position sourcePosition, final Position targetPosition) {
+		final int subtractX = sourcePosition.getX() - targetPosition.getX();
+		final int subtractY = sourcePosition.getY() - targetPosition.getY();
 
+		if (!checkRecursive(subtractX, subtractY, 1)) {
+			throw new IllegalArgumentException(ExceptionMessage.PIECE_CAN_NOT_GO_DESTINATION_POSITION);
+		}
+	}
+
+	private boolean checkRecursive(final int subtractX, final int subtractY, final int multiplyNumber) {
+		if (multiplyNumber == 8)
+			return false;
+
+		final boolean isCanGo = directions.stream()
+			.anyMatch(direction -> {
+				final int xDegree = direction.getXDegree() * multiplyNumber;
+				final int yDegree = direction.getYDegree() * multiplyNumber;
+
+				return xDegree == subtractX && subtractY == yDegree;
+			});
+
+		if (!isCanGo) {
+			return checkRecursive(subtractX, subtractY, multiplyNumber + 1);
+		}
+
+		return true;
 	}
 }
