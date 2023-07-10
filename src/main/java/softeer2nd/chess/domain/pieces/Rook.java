60e9d8d15d3c5e2b3d1267411ad30f6eae2311e0
kwyoohae <gocksdl7979@gmail.com>
2023-07-10 12:10:14 +0900

    feat: Rook이 이동가능한지 확인하는 로직 작성

--- src/main/java/softeer2nd/chess/domain/pieces/Rook.java
+++ src/main/java/softeer2nd/chess/domain/pieces/Rook.java
@@ -1,6 +1,7 @@
 package softeer2nd.chess.domain.pieces;
 
 import softeer2nd.chess.domain.board.position.Position;
+import softeer2nd.chess.exception.ExceptionMessage;
 
 public class Rook extends Piece{
 
@@ -8,10 +9,34 @@ public class Rook extends Piece{
 		this.color = color;
 		this.type = type;
 		this.position = position;
+		this.directions = Direction.linearDirection();
 	}
 
 	@Override
 	public void checkPieceCanGo(final Position sourcePosition, final Position targetPosition) {
+		final int subtractX = sourcePosition.getX() - targetPosition.getX();
+		final int subtractY = sourcePosition.getY() - targetPosition.getY();
 
+		if (!checkRecursive(subtractX, subtractY, 1)) {
+			throw new IllegalArgumentException(ExceptionMessage.PIECE_CAN_NOT_GO_DESTINATION_POSITION);
+		}
+	}
+
+	private boolean checkRecursive(final int x, final int y, final int multiplyNumber) {
+		if (multiplyNumber == 8)
+			return false;
+
+		final boolean isCanGo = directions.stream()
+			.anyMatch(direction -> {
+				final int xDegree = direction.getXDegree() * multiplyNumber;
+				final int yDegree = direction.getYDegree() * multiplyNumber;
+
+				return xDegree == x && yDegree == y;
+			});
+
+		if (!isCanGo) {
+			return checkRecursive(x, y, multiplyNumber + 1);
+		}
+		return true;
 	}
 }
