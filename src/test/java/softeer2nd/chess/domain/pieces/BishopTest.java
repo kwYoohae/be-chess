c5be5905f442f0ee097852b6fa99e8aeebcc5cf7
kwyoohae <gocksdl7979@gmail.com>
2023-07-10 13:13:37 +0900

    test: 비숍은 대각선으로 가면 제대로 이동하는 테스트 작성

---
+++ src/test/java/softeer2nd/chess/domain/pieces/BishopTest.java
@@ -0,0 +1,39 @@
+package softeer2nd.chess.domain.pieces;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import softeer2nd.chess.domain.board.Board;
+import softeer2nd.chess.domain.board.position.Position;
+
+class BishopTest {
+
+	private Board board;
+
+	@BeforeEach
+	void beforeEach() {
+		board = new Board();
+	}
+
+	@ParameterizedTest
+	@DisplayName("비숍은 대각선 어디든 움직일 수 있다")
+	@ValueSource(strings = {"c5", "c3", "e3", "e5", "a7", "a1","g1","h8"})
+	void canGo(String destination) {
+		// given
+		board.initializeEmpty();
+
+		String position = "d4";
+		final Piece bishop = Piece.createBishop(Piece.Color.WHITE, new Position(position));
+		board.move(position, bishop);
+
+		// when
+		board.move(position, destination);
+
+		// then
+		assertThat(board.findPiece(destination)).isEqualTo(bishop);
+	}
+}
