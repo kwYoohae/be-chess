3c0a7b0a086717c1b54616fe140228da1cbc7fcd
kwyoohae <gocksdl7979@gmail.com>
2023-07-10 17:22:25 +0900

    test: 처음에는 폰이 2칸을 갈 수 있는것을 확인는 테스트 작성

---
+++ src/test/java/softeer2nd/chess/domain/pieces/PawnTest.java
@@ -0,0 +1,35 @@
+package softeer2nd.chess.domain.pieces;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import softeer2nd.chess.domain.board.Board;
+import softeer2nd.chess.domain.board.position.Position;
+
+class PawnTest {
+
+	private Board board;
+
+	@BeforeEach
+	void beforeEach() {
+		board = new Board();
+	}
+
+	@Test
+	@DisplayName("폰은 처음에는 2칸을 갈 수 있다")
+	void firstMoveSecondNorthOrSouth() {
+		// given
+		board.initialize();
+
+		// when
+		board.move("b2", "b4");
+		board.move("b7", "b5");
+
+		//then
+		assertThat(board.findPiece("b4")).isEqualTo(Piece.createPawn(Piece.Color.WHITE, new Position("b4")));
+		assertThat(board.findPiece("b5")).isEqualTo(Piece.createPawn(Piece.Color.BLACK, new Position("b5")));
+	}
+}
