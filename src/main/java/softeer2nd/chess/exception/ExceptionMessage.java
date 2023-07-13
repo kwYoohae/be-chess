067dec7e4d839ced4852c933819d0af46746dd02
kwyoohae <gocksdl7979@gmail.com>
2023-07-13 15:31:01 +0900

    refactor: 안쓰는 메시지 삭제

--- src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
+++ src/main/java/softeer2nd/chess/exception/ExceptionMessage.java
@@ -2,7 +2,6 @@ package softeer2nd.chess.exception;
 
 public final class ExceptionMessage {
 	public static final String EXCEPTION_MESSAGE_CLASS_MUST_NOT_INSTANTIATE = "ExceptionMessage는 인스턴스화 할 수 없는 유틸 클래스입니다";
-	public static final String NOT_EXECUTE_COMMAND = "실행 할 수 없는 명령어 입니다";
 	public static final String NOT_EXIST_PIECE = "존재하지 않는 기물입니다";
 	public static final String POSITION_INPUT_IS_TO_MANY_OR_LESS_LENGTH = "위치의 값을 너무 많이 또는 적게 입력하셨습니다";
 	public static final String POSITION_INPUT_IS_WRONG = "위치의 값은 a~h + 1~8만 될 수 있습니다 (ex. a8)";
