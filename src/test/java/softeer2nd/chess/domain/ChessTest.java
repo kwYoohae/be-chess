452e1baec1fad86697a93c31f297f2bdf0dbb9c1
kwyoohae <gocksdl7979@gmail.com>
2023-07-14 09:15:24 +0900

    test: 화이트 킹이 잡힐 위기인지 판단하는 테스트 작성

--- src/test/java/softeer2nd/chess/domain/ChessTest.java
+++ src/test/java/softeer2nd/chess/domain/ChessTest.java
@@ -1,6 +1,7 @@
 package softeer2nd.chess.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +12,19 @@ import softeer2nd.chess.domain.pieces.component.Color;
 
 class ChessTest {
 
+	private Board board;
+	private Chess chess;
+
+	@BeforeEach
+	void beforeEach() {
+		board = new Board(new BoardInitializer());
+		chess = new Chess(board);
+	}
+
 	@Test
 	@DisplayName("블랙 킹이 잡힐 위기면 check라고 값을 반환한다")
 	void blackCheck() {
 		// given
-		final Board board = new Board(new BoardInitializer());
-		Chess chess = new Chess(board);
 		board.initializeEmpty();
 		board.addPiece("d4", Piece.createPiece(Color.WHITE, Piece.Type.QUEEN));
 		board.addPiece("f3", Piece.createPiece(Color.WHITE, Piece.Type.KING));
@@ -29,5 +37,23 @@ class ChessTest {
 		// when then
 		Assertions.assertThat(chess.isChecked(Color.WHITE)).isTrue();
 		Assertions.assertThat(chess.isChecked(Color.BLACK)).isFalse();
+	}
+
+	@Test
+	@DisplayName("화이트 킹이 잡힐 위기면 check라고 값을 반환한다")
+	void whhiteCheck() {
+		// given
+		board.initializeEmpty();
+		board.addPiece("d4", Piece.createPiece(Color.WHITE, Piece.Type.QUEEN));
+		board.addPiece("f3", Piece.createPiece(Color.WHITE, Piece.Type.KING));
+		board.addPiece("g6", Piece.createPiece(Color.BLACK, Piece.Type.QUEEN));
+		board.addPiece("e8", Piece.createPiece(Color.BLACK, Piece.Type.KING));
+
+		// chess.movePiece("d4", "d8", Color.WHITE);
+		chess.movePiece("d4", "e4", Color.WHITE);
+
+		// when then
+		Assertions.assertThat(chess.isChecked(Color.WHITE)).isFalse();
+		Assertions.assertThat(chess.isChecked(Color.BLACK)).isTrue();
 	}
 }
